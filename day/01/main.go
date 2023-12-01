7343d33909332daa9ac1342c20227689c22e1f0a
Tanmay Satam <tsatam93@gmail.com>
2023-12-01 06:49:35 -0500

    day 01 part 02

--- day/01/main.go
+++ day/01/main.go
@@ -16,12 +16,20 @@ var (
 )
 
 func main() {
-	fmt.Printf("%d\n", handle(input))
+	fmt.Printf("Part 1: %d\n", handlePart1(input))
+	fmt.Printf("Part 2: %d\n", handlePart2(input))
 }
 
-func handle(input string) int {
+func handlePart1(input string) int {
 	lines := readInput(input)
 	calibrationValues := fp.Map(lines, calibrationValueForLine)
+	sum := fp.Reduce(calibrationValues, 0, func(curr, next int) int { return curr + next })
+	return sum
+}
+
+func handlePart2(input string) int {
+	lines := readInput(input)
+	calibrationValues := fp.Map(lines, calibrationValueForLinePart2)
 	sum := fp.Reduce(calibrationValues, 0, func(curr, next int) int { return curr + next })
 	return sum
 }
@@ -44,4 +52,49 @@ func calibrationValueForLine(line string) int {
 	}
 
 	return int(res)
+}
+
+func calibrationValueForLinePart2(line string) int {
+	if len(line) == 0 {
+		return 0
+	}
+
+	firstIdx := len(line)
+	firstVal := 0
+	lastIdx := -1
+	lastVal := 0
+
+	firstIdxLiteral := strings.IndexFunc(line, unicode.IsNumber)
+	if firstIdxLiteral >= 0 {
+		firstIdx = firstIdxLiteral
+		firstValTemp, err := strconv.ParseInt(string(line[firstIdxLiteral]), 10, 0)
+		if err != nil {
+			panic(err)
+		}
+		firstVal = int(firstValTemp)
+	}
+	lastIdxLiteral := strings.LastIndexFunc(line, unicode.IsNumber)
+	if lastIdxLiteral >= 0 {
+		lastIdx = lastIdxLiteral
+		lastValTemp, err := strconv.ParseInt(string(line[lastIdxLiteral]), 10, 0)
+		if err != nil {
+			panic(err)
+		}
+		lastVal = int(lastValTemp)
+	}
+	for i, thesearentdigits := range []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"} {
+		firstIdxNotDigit := strings.Index(line, thesearentdigits)
+		if firstIdxNotDigit >= 0 && firstIdxNotDigit < firstIdx {
+			firstIdx = firstIdxNotDigit
+			firstVal = i + 1
+		}
+
+		lastIdxNotDigit := strings.LastIndex(line, thesearentdigits)
+		if lastIdxNotDigit >= 0 && lastIdxNotDigit > lastIdx {
+			lastIdx = lastIdxNotDigit
+			lastVal = i + 1
+		}
+	}
+
+	return 10*firstVal + lastVal
 }
--- day/01/main_test.go
+++ day/01/main_test.go
@@ -5,14 +5,33 @@ import (
 	"testing"
 )
 
-func TestHandle(t *testing.T) {
+func TestHandlePart1(t *testing.T) {
 	input := `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
-treb7uchet`
+treb7uchet
+`
 
 	want := 142
-	got := handle(input)
+	got := handlePart1(input)
+
+	if got != want {
+		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func TestHandlePart2(t *testing.T) {
+	input := `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+`
+
+	want := 281
+	got := handlePart2(input)
 
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
@@ -32,6 +51,29 @@ func TestCalibrationValueForLine(t *testing.T) {
 	} {
 		t.Run(fmt.Sprintf("%s -> %d", tt.line, tt.want), func(t *testing.T) {
 			got := calibrationValueForLine(tt.line)
+			if got != tt.want {
+				t.Errorf("got %d, want %d", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCalibrationValueForLinePart2(t *testing.T) {
+	for _, tt := range []struct {
+		line string
+		want int
+	}{
+		{line: "", want: 0},
+		{line: "two1nine", want: 29},
+		{line: "eightwothree", want: 83},
+		{line: "abcone2threexyz", want: 13},
+		{line: "xtwone3four", want: 24},
+		{line: "4nineeightseven2", want: 42},
+		{line: "zoneight234", want: 14},
+		{line: "7pqrstsixteen", want: 76},
+	} {
+		t.Run(fmt.Sprintf("%s -> %d", tt.line, tt.want), func(t *testing.T) {
+			got := calibrationValueForLinePart2(tt.line)
 			if got != tt.want {
 				t.Errorf("got %d, want %d", got, tt.want)
 			}
