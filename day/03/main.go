0bbd996d35a121c516e20488b92174de52e8b814
Tanmay Satam <tsatam93@gmail.com>
2023-12-03 07:02:50 -0500

    day 03 part 02

--- day/03/main.go
+++ day/03/main.go
@@ -21,15 +21,23 @@ type Point struct {
 // coords retrieved via y,x
 type Schematic [][]rune
 type PartNumbers [][]int
+type GearRatios [][]int
 
 func main() {
 	fmt.Printf("Part 1: %d\n", handlePart1(input))
+	fmt.Printf("Part 2: %d\n", handlePart2(input))
 }
 
 func handlePart1(input string) int {
 	schematic := readInput(input)
 	partNumbers := getPartNumbers(schematic)
-	return sumPartNumbers(partNumbers)
+	return sum(partNumbers)
+}
+
+func handlePart2(input string) int {
+	schematic := readInput(input)
+	gearRatios := getGearRatios(schematic)
+	return sum(gearRatios)
 }
 
 func readInput(input string) Schematic {
@@ -91,10 +99,67 @@ func setPartNumbersAroundSymbol(s Schematic, p Point, partNumbers PartNumbers) {
 	}
 }
 
-func sumPartNumbers(p PartNumbers) int {
-	return fp.Sum(fp.Map(p, fp.Sum))
+func sum(m [][]int) int {
+	return fp.Sum(fp.Map(m, fp.Sum))
 }
 
 func isSymbol(r rune) bool {
 	return !unicode.IsDigit(r) && r != '.'
+}
+
+func getGearRatios(s Schematic) GearRatios {
+	gearRatios := make(GearRatios, len(s))
+	for y := range gearRatios {
+		gearRatios[y] = make([]int, len(s[y]))
+	}
+
+	for y, row := range s {
+		for x, r := range row {
+			if r == '*' {
+				setGearRatio(s, Point{X: x, Y: y}, gearRatios)
+			}
+		}
+	}
+	return gearRatios
+}
+
+func setGearRatio(s Schematic, gear Point, gearRatios GearRatios) {
+	partNumbersFound := map[Point]int{}
+
+	adjacent := []int{-1, 0, 1}
+	for _, dy := range adjacent {
+		for _, dx := range adjacent {
+			y := gear.Y + dy
+			x := gear.X + dx
+
+			outOfBounds := y < 0 || y >= len(s) || x < 0 || x >= len(s[y])
+			if outOfBounds || !unicode.IsDigit(s[y][x]) {
+				continue
+			}
+
+			partNumber := 0
+
+			for ; x < len(s[y]) && unicode.IsDigit(s[y][x]); x++ {
+			}
+			x--
+
+			base := 1
+
+			for ; x >= 0 && unicode.IsDigit(s[y][x]); x-- {
+				partNumber += base * int(s[y][x]-'0')
+				base *= 10
+			}
+			x++
+
+			partNumbersFound[Point{X: x, Y: y}] = partNumber
+		}
+	}
+
+	if len(partNumbersFound) == 2 {
+		result := 1
+		for _, n := range partNumbersFound {
+			result *= n
+		}
+		gearRatios[gear.Y][gear.X] = result
+	}
 }
